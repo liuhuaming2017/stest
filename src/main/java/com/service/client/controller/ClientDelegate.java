653dcc591c1ed05dc8132de8575c17d0b14f3570
liuhuaming2017 <33945540+liuhuaming2017@users.noreply.github.com>
2017-12-23 17:05:59 +0800

    Update ClientDelegate.java

--- src/main/java/com/service/client/controller/ClientDelegate.java
+++ src/main/java/com/service/client/controller/ClientDelegate.java
@@ -1,13 +1,16 @@
 package com.service.client.controller;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import io.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
 
 
 @Component
 public class ClientDelegate {
 
+    private static RestTemplate restTemplate = RestTemplateBuilder.create();
     public String helloworld(String name){
-
+        String name =restTemplate.getForObject("cse://apigteam/Helloworld?name="+name, null, String.class);
         // Do Some Magic Here!
         return name;
     }
